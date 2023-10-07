d3b67d8f23eba00e551d6d477a1a994821d430bf
satyam0002 <106262613+satyam0002@users.noreply.github.com>
2023-10-07 11:20:21 +0530

    fixed upload.js

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 import { GridFsStorage } from 'multer-gridfs-storage';
 
 const storage = new GridFsStorage({
-    url: `mongodb://satyamfoodit:fooditSa@ac-frc9v4i-shard-00-00.knj8og2.mongodb.net:27017,ac-frc9v4i-shard-00-01.knj8og2.mongodb.net:27017,ac-frc9v4i-shard-00-02.knj8og2.mongodb.net:27017/?ssl=true&replicaSet=atlas-u8oz9v-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    url: `mongodb://${username}:${password}@ac-frc9v4i-shard-00-00.knj8og2.mongodb.net:27017,ac-frc9v4i-shard-00-01.knj8og2.mongodb.net:27017,ac-frc9v4i-shard-00-02.knj8og2.mongodb.net:27017/?ssl=true&replicaSet=atlas-u8oz9v-shard-0&authSource=admin&retryWrites=true&w=majority`,
     options: { useNewUrlParser: true },
     file: (request, file) => {
         const match = ["image/png", "image/jpg"];
